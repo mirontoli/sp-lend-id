a9bc63c66b7b4572d03a69132ea3259c01d27cf9
Anatoly Mironov <mirontoli@gmail.com>
2012-04-08 00:40:21 +0200

    added test for feature activation

--- taprat/test/get-demonames.ps1
+++ taprat/test/get-demonames.ps1
@@ -1,9 +1,13 @@
- Import-Module C:\code\taprat\cmdlet\sp-lend-id.patrat\bin\Debug\sp-lend-id.patrat.dll
- Get-Module
- Get-DemoNames
+Import-Module C:\code\sp-lend-id\taprat\cmdlet\sp-lend-id.taprat\bin\Debug\sp-lend-id.patrat.dll
+Get-Module
+Get-DemoNames
  #should output
  
  #Tolle
  #Sinan
  #Frissan
  #Ekan
+ 
+ 
+$p = @{"Test" = "Tjena" }
+Enable-SPFeatureWithProperties -Identity "b5eef7d1-f46f-44d1-b53e-410f62032846" -Url "http://dev" -Properties $p
