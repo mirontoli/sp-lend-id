f983805f4bd2b0c7093a366645a79ca16ae5af89
Anatoly Mironov <mirontoli@gmail.com>
2012-04-07 16:17:09 +0200

    tested adding in console

--- taprat/cmdlet/sp-lend-id.taprat.console/SPFeatureExtensions.cs
+++ taprat/cmdlet/sp-lend-id.taprat.console/SPFeatureExtensions.cs
@@ -24,19 +24,33 @@ namespace sp_lend_id.taprat.console
             return ActivateFeature(features, featureId, properties);
         }
         private static SPFeature ActivateFeature(this SPFeatureCollection features, Guid featureId, SPFeaturePropertyCollection properties)
-        {
-            var getFeatureInternal = typeof(SPFeatureCollection)
-                .GetMethod("GetFeature", BindingFlags.Instance | BindingFlags.NonPublic, null,
-                    new[] { typeof(Guid) }, null);
-            var alreadyActivatedFeature = (SPFeature)getFeatureInternal
-                .Invoke(features, new object[] { featureId });
-            if (alreadyActivatedFeature != null)
+        {          
+            if (features[featureId] != null)
+            {
                 // The feature is already activated. No action required
                 return null;
-            var addInternal = typeof(SPFeatureCollection)
-                .GetMethod("Add", BindingFlags.Instance | BindingFlags.NonPublic, null,
-                    new[] { typeof(Guid), typeof(SPFeaturePropertyCollection), typeof(bool) }, null);
-            var result = addInternal.Invoke(features, new object[] { featureId, properties, false });
+            }
+            var type = typeof(SPFeatureCollection);
+            //var getFeatureInternal = type.GetMethod("GetFeature", BindingFlags.Instance | BindingFlags.NonPublic, null,
+            //        new[] { typeof(Guid) }, null);
+            //var alreadyActivatedFeature = (SPFeature)getFeatureInternal
+            //    .Invoke(features, new object[] { featureId });
+            //if (alreadyActivatedFeature != null)
+            //    // The feature is already activated. No action required
+            //    return null;
+
+            //var addInternal = type.GetMethod("Add", BindingFlags.Instance | BindingFlags.NonPublic, null,
+            //        new Type[] { typeof(Guid), typeof(SPFeaturePropertyCollection), typeof(bool) }, null);
+            //object result = null;
+            //if (addInternal != null)
+            //{
+            //    result = addInternal.Invoke(features, new object[] { featureId, properties, false });
+            //}
+            
+            var addInternal = type.GetMethod("AddInternal", BindingFlags.Instance | BindingFlags.NonPublic, null,
+                    new Type[] { typeof(Guid), typeof(Version), typeof(SPFeaturePropertyCollection), typeof(bool), typeof(bool), typeof(SPFeatureDefinitionScope) }, null);
+            //AddInternal(Guid featureId, Version version, SPFeaturePropertyCollection properties, bool force, bool fMarkOnly, SPFeatureDefinitionScope featdefScope)
+            var result = addInternal.Invoke(features, new object[] { featureId, null, properties, false, false, SPFeatureDefinitionScope.Farm });
             return result as SPFeature;
         }
     }
