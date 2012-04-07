0576c5ca07be8882ce4762594e24488e705944fc
Anatoly Mironov <mirontoli@gmail.com>
2012-04-07 15:14:15 +0200

    added console app for trying things

---
+++ taprat/cmdlet/sp-lend-id.taprat.console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using Microsoft.SharePoint;
+
+namespace sp_lend_id.taprat.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
