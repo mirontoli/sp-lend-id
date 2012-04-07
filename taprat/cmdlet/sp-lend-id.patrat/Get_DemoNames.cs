1324eac4427144b3c365bf09defc5ddd59da18e6
Anatoly Mironov <mirontoli@gmail.com>
2012-04-07 14:58:48 +0200

    Created Get-DemoNames

--- taprat/cmdlet/sp-lend-id.patrat/Get_DemoNames.cs
+++ taprat/cmdlet/sp-lend-id.patrat/Get_DemoNames.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace sp_lend_id.patrat
+﻿namespace sp_lend_id.patrat
 {
-    public class Get_DemoNames
+    /// <summary>
+    /// This class is just a little example:
+    /// http://blogs.msdn.com/b/saveenr/archive/2010/03/08/how-to-create-a-powershell-2-0-module-and-cmdlet-with-visual-studio-2010-screencast-included.aspx
+    /// </summary>
+    [System.Management.Automation.Cmdlet(System.Management.Automation.VerbsCommon.Get, "DemoNames")]
+    public class Get_DemoNames : System.Management.Automation.PSCmdlet
     {
+        protected override void ProcessRecord()
+        {
+            var names = new[] {"Tolle", "Sinan", "Frissan", "Ekan"};
+            WriteObject(names);
+        }
     }
 }
