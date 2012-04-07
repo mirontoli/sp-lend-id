1557f98b0e2d9775e0779f723a5dec1771eaf7d7
Anatoly Mironov <mirontoli@gmail.com>
2012-04-08 00:40:50 +0200

    converting hashtable to dictionary

--- taprat/cmdlet/sp-lend-id.taprat/Enable_SPFeatureWithProperties.cs
+++ taprat/cmdlet/sp-lend-id.taprat/Enable_SPFeatureWithProperties.cs
@@ -1,16 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Microsoft.SharePoint;
 
 namespace sp_lend_id.taprat
 {
     [System.Management.Automation.Cmdlet(System.Management.Automation.VerbsLifecycle.Enable, "SPFeatureWithProperties")]
     public class Enable_SPFeatureWithProperties : System.Management.Automation.PSCmdlet
     {
+        /// <summary>
+        /// Id for the feature
+        /// For now only guid supported
+        /// </summary>
+        /// todo add support for name
+        [System.Management.Automation.Parameter(Position = 0, Mandatory = true)] 
+        public string Identity;
+        
+        [System.Management.Automation.Parameter(Position = 1, Mandatory = true)]
+        public string Url;
+
+        [System.Management.Automation.Parameter(Position = 2, Mandatory = false)]
+        public System.Collections.Hashtable Properties; 
+
         protected override void ProcessRecord()
         {
-            base.ProcessRecord();
+            var properties = GetProperties();
+            WriteObject(properties);
+            using (var site = new SPSite(Url))
+            {
+                using (var web = site.OpenWeb())
+                {
+                    var features = web.Features;
+                    var id = new Guid(Identity);
+                    features.ActivateFeature(id, properties);
+                }
+            }
+        }
+        /// <summary>
+        /// Converts hashtable to dictionary
+        /// http://stackoverflow.com/questions/6455822/convert-hashtable-to-dictionary-in-c-sharp/6455939#6455939
+        /// no error handling for now
+        /// </summary>
+        /// <returns></returns>
+        private Dictionary<string, string> GetProperties()
+        {
+            var dictionary = new Dictionary<string, string>();
+            if (Properties != null)
+            {
+                foreach (var key in Properties.Keys)
+                {
+                    dictionary.Add((string)key, (string)Properties[key]);
+                }
+            }
+            return dictionary;
         }
     }
 }
