f52d9a20602df58391a8b8fa0ff59abd0010da2b
Anatoly Mironov <mirontoli@gmail.com>
2012-04-07 15:37:35 +0200

    changed Program.Main

--- taprat/cmdlet/sp-lend-id.taprat.console/Program.cs
+++ taprat/cmdlet/sp-lend-id.taprat.console/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
 using Microsoft.SharePoint;
 
 namespace sp_lend_id.taprat.console
@@ -8,6 +8,15 @@ namespace sp_lend_id.taprat.console
     {
         static void Main(string[] args)
         {
+            using (var site = new SPSite("http://dev"))
+            {
+                using (var web = site.OpenWeb())
+                {
+                    var activationProps = new Dictionary<string, string>
+                                              {{"Test", "Title changed by feature in activation"}};
+                    web.Features.ActivateFeature(new Guid("b5eef7d1-f46f-44d1-b53e-410f62032846"), activationProps);
+                }
+            }
         }
     }
 }
