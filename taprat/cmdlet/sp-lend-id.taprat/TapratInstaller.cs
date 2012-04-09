babfb4b1a715cbf4c835b07951404128c90ee71a
Anatoly Mironov <mirontoli@gmail.com>
2012-04-09 16:27:13 +0200

    #3 create pssnapin

---
+++ taprat/cmdlet/sp-lend-id.taprat/TapratInstaller.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Management.Automation;
+
+namespace sp_lend_id.patrat
+{
+    [RunInstaller(true)]
+    public class TapratInstaller : PSSnapIn
+    {
+        public override string Name
+        {
+            get
+            {
+                return "sp-lend-id.taprat";
+            }
+        }
+
+        public override string Vendor
+        {
+            get 
+            { 
+                return "Anatoly Mironov";
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return
+                    "This includes an experimental cmdlet to activate sharepoint features and specify custom properties";
+            }
+        }
+    }
+}
