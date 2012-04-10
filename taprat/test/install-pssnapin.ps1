4c0d0b3c1a1a35c510828779b57dfd57f78be205
Anatoly Mironov <mirontoli@gmail.com>
2012-04-10 21:20:56 +0200

    added installation script

---
+++ taprat/test/install-pssnapin.ps1
@@ -0,0 +1,2 @@
+Set-Alias installutil $env:windir\Microsoft.NET\Framework64\v2.0.50727\installutil.exe
+installutil C:\code\sp-lend-id\taprat\cmdlet\sp-lend-id.taprat\bin\Debug\sp-lend-id.taprat.dll
