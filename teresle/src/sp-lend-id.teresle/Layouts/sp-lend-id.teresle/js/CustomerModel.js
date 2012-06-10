9ada5c98e4c0332820a908e1c2b2d3bce6b01cac
Anatoly Mironov <mirontoli@gmail.com>
2012-06-10 14:53:09 +0200

    nodeunit test on server

---
+++ teresle/src/sp-lend-id.teresle/Layouts/sp-lend-id.teresle/js/CustomerModel.js
@@ -0,0 +1,15 @@
+﻿function CustomerModel(firstName, lastName, city) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.city = city;
+}
+
+CustomerModel.prototype = {
+    getFullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
+};
+// Node
+if (typeof module !== 'undefined') {
+    module.exports = CustomerModel;
+}
---
+++ teresle/src/sp-lend-id.teresle/Layouts/sp-lend-id.teresle/js/nodeunit-test/server/TestCustomerModel.js
@@ -0,0 +1,8 @@
+﻿var CustomerModel = require("../../CustomerModel.js");
+exports.testFullName = function (test) {
+    test.expect(1);
+    var customer = new CustomerModel("Konstantin", "Ivanov", "Slakbash");
+    var fullName = customer.getFullName();
+    test.equal(fullName, "Konstantin Ivanov", "Fullname = firstname lastname");
+    test.done();
+}
