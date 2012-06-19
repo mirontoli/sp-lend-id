d2a2028a73dd96db9a146de10741657b8bd5a655
Anatoly Mironov <mirontoli@gmail.com>
2012-06-19 23:08:44 +0200

    added tupsam

---
+++ tupsam/src/sp-lend-id.tupsam/Layouts/sp-lend-id/tupsam/properties.ashx
@@ -0,0 +1,3 @@
+﻿<%@ Assembly Name="Microsoft.SharePoint, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Assembly Name="sp-lend-id.tupsam, Version=1.0.0.0, Culture=neutral, PublicKeyToken=40dfa473d220aaaf" %>
+<%@ WebHandler Language="C#"  Class="sp_lend_id.tupsam.Properties2JsHandler" %>
---
+++ tupsam/src/sp-lend-id.tupsam/Properties2JsHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Web;
+using Microsoft.SharePoint;
+
+namespace sp_lend_id.tupsam
+{
+    public class Properties2JsHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+        public void ProcessRequest(HttpContext context)
+        {
+            var js = GetJavascript(context.Request.Url.ToString());
+            context.Response.ContentType = "application/x-javascript";
+            context.Response.Write(js);
+        }
+
+        private static string GetJavascript(string url)
+        {
+            var json = string.Empty;
+            var properties = GetProperties(url);
+            if (properties != null)
+            {
+                var pairs =
+                    (from DictionaryEntry prop in properties
+                     select string.Format("\t\"{0}\" : \"{1}\"", prop.Key, prop.Value)).ToList();
+
+                var s = string.Join(",\n", pairs.ToArray());
+                json = string.Format("_webAppProperties = {{\n{0}\n}}", s);
+
+            }
+            return json;
+        }
+
+        private static Hashtable GetProperties(string url)
+        {
+            Hashtable properties = null;
+            SPSecurity.RunWithElevatedPrivileges(() =>
+                    {
+                        using (var site = new SPSite(url))
+                        {
+                            var app = site.WebApplication;
+                            properties = app.Properties;
+                        }
+                    });
+            return properties;
+        }
+    }
+}
