3dba971642a0f00f082b37557a872deff4468be7
Anatoly Mironov <mirontoli@gmail.com>
2013-06-17 00:18:42 +0200

    Add replacecontent for js file

--- pecek/COB.SharePoint.AccordionList/SiteAssets/AccordionListView.js
+++ pecek/COB.SharePoint.AccordionList/SiteAssets/AccordionListView.js
@@ -1,16 +1,15 @@
-﻿
-// function to setup JSLink templates
-function registerAccordion() {
-    // function to process an accordion item..
-    window.COB = window.COB || {};
-    window.COB.accordionItem = {
-        customItemHtml: function (ctx) {
-            var accordionItemHtml = "<h3>" + ctx.CurrentItem.Title + "</h3>";
-            accordionItemHtml += "<div>" + ctx.CurrentItem.AccordionItemDescription + "</div>";
-            return accordionItemHtml;
-        }
-    };
+﻿// function to process an accordion item..
+window.COB = window.COB || {};
+window.COB.accordionItem = {
+    customItemHtml: function (ctx) {
+        var accordionItemHtml = "<h3>" + ctx.CurrentItem.Title + "</h3>";
+        accordionItemHtml += "<div>" + ctx.CurrentItem.AccordionItemDescription + "</div>";
+        return accordionItemHtml;
+    }
+};
 
+// anonymous self-executing function to setup JSLink templates on page load..
+(function () {
     var overrideCtx = {};
     overrideCtx.Templates = {};
 
@@ -22,25 +21,15 @@ function registerAccordion() {
     overrideCtx.ListTemplateType = 11000;
 
     SPClientTemplates.TemplateManager.RegisterTemplateOverrides(overrideCtx);
+})();
 
-    $(document).ready(function () {
-        // It seems SharePoint inserts a script tag in an inconvenient place that breaks jQuery UI's accordion, so let's remove it! 
-        // (N.B. further testing recommended for production)..
-        $("#accordion").find('#scriptBodyWPQ2').remove();
+function onReady() {
+    // It seems SharePoint inserts a script tag in an inconvenient place that breaks jQuery UI's accordion, so let's remove it! 
+    // (N.B. further testing recommended for production)..
+    $("#accordion").find('#scriptBodyWPQ2').remove();
 
-        $("#accordion").width('70%');
-        $("#accordion").accordion();
-    });
-
+    $("#accordion").width('70%');
+    $("#accordion").accordion();
 }
 
-
-
-
-
-//CSR-override for MDS disabled site (because we need to call the entry point function in this case whereas it is not needed for anonymous functions)
-registerAccordion();
-
-// http://blogs.msdn.com/b/sridhara/archive/2013/02/08/register-csr-override-on-mds-enabled-sharepoint-2013-site.aspx
-// CSR-override for MDS enabled site
-RegisterModuleInit("/SiteAssets/AccordionListView.js", registerAccordion);
+_spBodyOnLoadFunctionNames.push("onReady");
